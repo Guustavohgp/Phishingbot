6fcd71ff47476cf5373ba96852889fd20b438633
Guustavohgp <gustavo.henriquegpires@gmail.com>
2025-10-18 18:53:53 -0300

    att logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,36 +1,23 @@
 import logging
-import sys
-from pathlib import Path
+import os
 
-LOG_DIR = Path("logs")
-LOG_DIR.mkdir(exist_ok=True)
+def get_logger(name: str = "phishing_logger") -> logging.Logger:
+    os.makedirs("logs", exist_ok=True)
+    logger = logging.getLogger(name)
 
-LOG_FILE = LOG_DIR / "app.log"
+    if not logger.hasHandlers():
+        logger.setLevel(logging.INFO)
+        fh = logging.FileHandler("logs/phishing.log")
+        ch = logging.StreamHandler()
 
-# Configuração do logger
-logger = logging.getLogger("phishing_detector")
-logger.setLevel(logging.DEBUG)  # DEBUG em dev, INFO em produção
+        formatter = logging.Formatter(
+            "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+            "%Y-%m-%d %H:%M:%S",
+        )
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
 
-# Handler para console
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setLevel(logging.INFO)
-console_formatter = logging.Formatter(
-    "[%(asctime)s] [%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
-)
-console_handler.setFormatter(console_formatter)
+        logger.addHandler(fh)
+        logger.addHandler(ch)
 
-# Handler para arquivo
-file_handler = logging.FileHandler(LOG_FILE, encoding="utf-8")
-file_handler.setLevel(logging.DEBUG)
-file_formatter = logging.Formatter(
-    "[%(asctime)s] [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
-)
-file_handler.setFormatter(file_formatter)
-
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
-
-# Exemplo de uso:
-# from your_package.logger import logger
-# logger.info("Iniciando análise de emails")
+    return logger
