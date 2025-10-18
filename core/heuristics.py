0763c298d93e5ba36c8ec28cd0914793fbe92295
Guustavohgp <gustavo.henriquegpires@gmail.com>
2025-10-18 18:55:06 -0300

    att heuristics

--- core/heuristics.py
+++ core/heuristics.py
@@ -1,36 +1,34 @@
 import tldextract
 from urlextract import URLExtract
-from config.settings import SUSPICIOUS_TLDS, SUSPICIOUS_DOMAINS, SENSITIVE_KEYWORDS
+from config import settings
 
 def check_phishing_heuristics(subject: str, body: str):
     score = 0
     reasons = []
-
     text = (subject or "") + "\n" + (body or "")
     text_lower = text.lower()
     extractor = URLExtract()
     urls = extractor.find_urls(text)
 
     for u in urls:
-        parsed = tldextract.extract(u)
-        root = parsed.domain + ('.' + parsed.suffix if parsed.suffix else '')
-        tld = parsed.suffix
-        if u in SUSPICIOUS_DOMAINS or root in SUSPICIOUS_DOMAINS:
+        root = f"{tldextract.extract(u).domain}.{tldextract.extract(u).suffix}"
+        tld = tldextract.extract(u).suffix
+        if u in settings.SUSPICIOUS_DOMAINS or root in settings.SUSPICIOUS_DOMAINS:
             score += 5
             reasons.append(f"Domínio suspeito: {u}")
-        if tld in SUSPICIOUS_TLDS:
+        if tld in settings.SUSPICIOUS_TLDS:
             score += 1
             reasons.append(f"TLD suspeito: .{tld}")
 
     context_words = ["envie", "informe", "resgatar", "clique", "confirme", "forneça"]
-    for word in SENSITIVE_KEYWORDS:
+    for word in settings.SENSITIVE_KEYWORDS:
         if word.lower() in text_lower and any(cw in text_lower for cw in context_words):
             score += 5
-            reasons.append(f"Solicita dados pessoais: '{word}'")
+            reasons.append(f"Solicita dados: '{word}'")
 
-    urgency_words = ["pague agora","bloqueio da conta","verifique sua conta","senha expirada"]
-    if any(word in text_lower for word in urgency_words):
+    urgency = ["pague agora", "bloqueio da conta", "verifique sua conta", "senha expirada"]
+    if any(u in text_lower for u in urgency):
         score += 3
-        reasons.append("Mensagem com urgência ou pagamento imediato.")
+        reasons.append("Mensagem urgente ou de bloqueio")
 
     return score, reasons
